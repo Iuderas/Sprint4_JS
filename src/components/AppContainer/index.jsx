8f882b939f6ab9d1fffc2cb182d8b13b9af1f504
Lucas Umada <lumada@doris.mobi>
2023-11-10 14:50:40 -0300

    create footer login problem and solve pages

--- src/components/AppContainer/index.jsx
+++ src/components/AppContainer/index.jsx
@@ -4,19 +4,34 @@ import * as S from "./styles";
 import logo from "../../images/logo.png";
 import { TextButton } from "../TextButton";
 
+import problem1 from "../../images/problem1.png";
+import problem2 from "../../images/problem2.png";
+import problem3 from "../../images/problem3.png";
+
+import solveSVG from "../../images/solve2.svg";
+
+const data = {
+  images: [
+    { id: 1, image: problem1 },
+    { id: 2, image: problem2 },
+    { id: 3, image: problem3 },
+  ],
+};
+
 export const AppContainer = () => {
   return (
     <>
       <S.HomeSection>
-        <S.HomeBackgroundShadow />
         <Header srcLogo={logo} />
 
         <S.HomeBrand>
           <S.BrandLogo src={logo} />
           <S.BrandTitle>URBAN FLOW</S.BrandTitle>
         </S.HomeBrand>
-        <S.Description>O transporte publico como você nunca viu.</S.Description>
-        <S.StyledButton onClick={() => {}}>Saiba mais</S.StyledButton>
+        <S.HomeDescription>
+          O transporte publico como você nunca viu.
+        </S.HomeDescription>
+        <TextButton onClick={() => {}}>Saiba mais</TextButton>
       </S.HomeSection>
 
       <S.ProblemSection>
@@ -25,7 +40,50 @@ export const AppContainer = () => {
           rastreando ônibus para reduzir lotação e tempo de espera, tornando a
           cidade mais sustentável.
         </S.Description>
+        <S.ProblemImagesContainer>
+          {data.images.map((item) => {
+            return <S.ProblemImage src={item.image} />;
+          })}
+        </S.ProblemImagesContainer>
       </S.ProblemSection>
+
+      <S.SolveSection>
+        <S.Description>
+          Urban Flow melhora o transporte público em São Paulo, monitorando e
+          rastreando ônibus para reduzir lotação e tempo de espera, tornando a
+          cidade mais sustentável.
+        </S.Description>
+
+        <S.SolveSVG src={solveSVG} />
+      </S.SolveSection>
+
+      <S.formSection>
+        <S.form>
+          <S.formTitle>CADASTRE-SE PARA NOVIDADES</S.formTitle>
+          <S.formDiv>
+            <S.formInput placeholder="nome:"></S.formInput>
+            <S.formInput placeholder="email:"></S.formInput>
+            <S.formButton>CONFIRMAR</S.formButton>
+          </S.formDiv>
+        </S.form>
+      </S.formSection>
+
+      <S.footerSection>
+        <S.footerContatos>
+          <S.contatos>
+            <S.contatosImg></S.contatosImg>
+            <S.contatosText>URBANFLOW@GMAIL.COM</S.contatosText>
+          </S.contatos>
+          <S.contatos>
+            <S.contatosImg></S.contatosImg>
+            <S.contatosText>(11)99999-9999</S.contatosText>
+          </S.contatos>
+          <S.contatos>
+            <S.contatosImg></S.contatosImg>
+            <S.contatosText>@URBAN_FLOW</S.contatosText>
+          </S.contatos>
+        </S.footerContatos>
+      </S.footerSection>
     </>
   );
 };
--- src/components/AppContainer/styles.js
+++ src/components/AppContainer/styles.js
@@ -1,30 +1,19 @@
 import styled from "styled-components";
 
-import homeBackground from "../../images/homebackground.jpeg";
-import { TextButton } from "../TextButton";
+import homeBackground from "../../images/homebackground.png";
+import solveBackground from "../../images/solve1.png";
 
 export const HomeSection = styled.section`
   display: flex;
   flex-direction: column;
   height: 100%;
   width: 100%;
-  background-image: url(${homeBackground});
+  background: url(${homeBackground}), rgb(0, 0, 0, 0.35);
+  background-size: cover;
   justify-content: center;
   align-items: center;
   overflow: hidden;
-`;
-
-export const StyledButton = styled(TextButton)`
-  margin-bottom: 148px;
-`;
-
-export const HomeBackgroundShadow = styled.div`
-  width: 100%;
-  height: 100%;
-  background-color: #000;
-  opacity: 0.35;
-  position: absolute;
-  z-index: 1;
+  padding-bottom: 79px;
 `;
 
 export const HomeBrand = styled.div`
@@ -32,29 +21,25 @@ export const HomeBrand = styled.div`
   flex-direction: column;
   align-items: center;
   margin-top: 145px;
-  z-index: 2;
 `;
 
 export const BrandLogo = styled.img`
   height: 100px;
   width: 100px;
-  z-index: 2;
 `;
 
 export const BrandTitle = styled.span`
   font-size: 56px;
   font-style: italic;
   color: #61e786;
-  z-index: 2;
 `;
 
-export const Description = styled.span`
+export const HomeDescription = styled.span`
   font-size: 36px;
   font-style: italic;
   color: #edffec;
   text-align: center;
   margin: 114px 0;
-  z-index: 2;
 `;
 
 export const ProblemSection = styled.div`
@@ -62,6 +47,118 @@ export const ProblemSection = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  padding: 90px 0;
   background-color: #48435c;
+`;
+
+export const Description = styled.span`
+  max-width: 850px;
+  font-size: 20px;
+  font-style: italic;
+  color: #edffec;
+  text-align: center;
+  margin: 114px 0;
+`;
+
+export const ProblemImagesContainer = styled.div`
+  display: flex;
+  height: 100%;
+  gap: 40px;
+  padding-bottom: 80px;
+`;
+
+export const ProblemImage = styled.img`
+  width: 30vh;
+  height: 30vh;
+  border-radius: 35px;
+  border: 4px solid #61e786;
+  object-fit: cover;
+`;
+
+export const SolveSection = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background: url(${solveBackground}), lightgray 50% / cover no-repeat !important;
+  padding: 100px;
+`;
+
+export const SolveSVG = styled.img`
+  max-width: 100%;
+  min-width: 30%;
+  padding-bottom: 110px;
+`;
+
+export const formSection = styled.div`
+  height: 80vh;
+  background-color: #48435c;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+export const form = styled.form`
+  border: 3px solid #61e786;
+  border-radius: 15px;
+  padding: 20px 55px 20px 55px;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const formTitle = styled.h2`
+  text-align: center;
+  color: #61e786;
+  border-bottom: 3px solid #61e786;
+`;
+
+export const formDiv = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  padding: 20px;
+`;
+export const formInput = styled.input`
+  margin-top: 1rem;
+  color: #fff;
+  width: 100%;
+  padding: 10px;
+  background-color: #48435c;
+  border: None;
+  border-bottom: 3px solid #61e786;
+`;
+
+export const formButton = styled.button`
+  margin-top: 3rem;
+  color: #fff;
+  padding: 20px 40px 20px 40px;
+  background-color: #61e786;
+  border-radius: 10px;
+  border: None;
+`;
+
+export const footerSection = styled.div`
+  height: 20vh;
+  display: flex;
+  align-items: center;
+  color: #fff;
+  background-color: #000;
+`;
+
+export const footerContatos = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin-left: 15px;
+`;
+
+export const contatos = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export const contatosImg = styled.img``;
+
+export const contatosText = styled.p`
+  margin-left: 10px;
+  margin-top: 15px;
 `;
